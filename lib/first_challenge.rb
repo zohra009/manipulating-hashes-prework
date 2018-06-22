221875cdcc8baedad131cdced008a515d9830b13
anzohra@outlook.com <anzohra@outlook.com>
2018-06-22 13:23:19 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,9 +13,13 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
+  contacts["Freddy Mercury"].each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"
+      end
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,7 @@ def second_challenge
   }
 
   #code your solution here!
-
+  groceries.values.flatten
   
 
 end
